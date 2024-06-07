33fe0cf07241ff63f8a02df13959bda19cf20685
jasmehark <155477413+jasmehark@users.noreply.github.com>
2024-06-07 12:10:10 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,45 @@
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output")})}
+Room_names = childKey;
+getData();
+
+function addRoom()
+{
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
+//Start code
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+//End code
+});});}
+getData();
+
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = ("kwitter.html");
+}
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
+
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location = ("kwitter.html");
+}
